33fef33684db687b403a192d138da1a4c94554aa
David Mella <david3e05@gmail.com>
2020-07-26 16:09:55 -0400

    changing and deleting files

--- not_compiled/hello.asm
+++ not_compiled/hello.asm
@@ -1,21 +1,35 @@
-; ----------------------------------------------------------------------------------------
-; Writes "Hello, World" to the console using only system calls. Runs on 64-bit Linux only.
-; To assemble and run:
-;
-;     nasm -felf64 hello.asm && ld hello.o && ./a.out
-; ----------------------------------------------------------------------------------------
+[BITS 16]
+[ORG 0x7C00]
 
-          global    _start
+MOV SI, BOOTLOADERSTR
+CALL printstring
+JMP $
 
-          section   .text
-_start:   mov       rax, 1                  ; system call for write
-          mov       rdi, 1                  ; file handle 1 is stdout
-          mov       rsi, message            ; address of string to output
-          mov       rdx, 13                 ; number of bytes
-          syscall                           ; invoke operating system to do the write
-          mov       rax, 60                 ; system call for exit
-          xor       rdi, rdi                ; exit code 0
-          syscall                           ; invoke operating system to exit
 
-          section   .data
-message:  db        "Hello, World", 10      ; note the newline at the end
+PrintCharacter:
+MOV AH, 0x0E
+MOV BH, 0x00
+MOV BL, 0x07
+ENT 0x10
+RET
+
+
+printstring
+next_character:
+MOV AL, [SI]
+INC SI
+OR AL, AL
+JZ exit_function
+CALL PrintCharacter
+exit_function
+RET
+
+;DATA
+BOOTLOADERSTR db 'F.C.E. 365 Bootloader for faoss v0' , 0
+
+TIMES 510 - ($ - $$)) db 0
+DW 0xAA55
+
+
+
+
