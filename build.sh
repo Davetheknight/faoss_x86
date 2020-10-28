ce202d25fb77afdea95bb07091d494a2e665b24a
Davetheknight <david3e05@gmail.com>
2020-10-28 16:57:47 -0400

    First version, probably v0.0000001

---
+++ build.sh
@@ -0,0 +1,5 @@
+nasm -f elf32 kernel.asm
+mv kernel.o kasm.o
+gcc -m32 -c kernel.c -o kc.o
+ld -m elf_i386 -T link.ld -o kernel.bin kasm.o kc.o
+qemu-system-x86_64 -kernel kernel.bin
---
+++ kernel.c
@@ -0,0 +1,7 @@
+kmain()
+{
+	char* vidmem = (char*)0xb8000;
+	vidmem[0] = 'A';
+	vidmem[1] = 0x07;
+
+}
