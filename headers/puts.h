4074ce021991eaf91a35bf688f6052571abf3964
David Mella <david3e05@gmail.com>
2021-01-08 14:34:41 -0500

    added print function for operaterating system

---
+++ headers/puts.h
@@ -0,0 +1,9 @@
+void puts(const char *string, int colour)
+{
+    volatile char *video = (volatile char*)0xB8000;
+    while( *string != 0 )
+    {
+        *video++ = *string++;
+        *video++ = colour;
+    }
+}
--- kernel.c
+++ kernel.c
@@ -1,7 +1,10 @@
-kmain()
+#include "headers/puts.h"
+int kmain()
 {
 	char* vidmem = (char*)0xb8000;
 	vidmem[0] = 'A';
 	vidmem[1] = 0x02;
+	puts("yes", 0x02);
 
+	return 0;
 }
