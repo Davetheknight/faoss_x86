12107e78f7cf2dd182064bcf9ba37940a010d8c1
David Mella <david3e05@gmail.com>
2021-01-09 18:50:20 -0500

    changed syntax

--- kernel.c
+++ kernel.c
@@ -1,10 +1,7 @@
 #include "headers/puts.h"
 int kmain()
 {
-	char* vidmem = (char*)0xb8000;
-	vidmem[0] = 'A';
-	vidmem[1] = 0x02;
-	puts("yes", 0x02);
+	puts("WELCOME TO FAOSS.", 0x02);
 
 	return 0;
 }
