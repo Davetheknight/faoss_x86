32aa979f52794f2467341024941bb34402e58699
David Mella <david3e05@gmail.com>
2021-02-23 16:11:45 -0500

    removed placeholder file, added new 'mains.c' file in kernel/shell which will have code on how the shell works and will be linked later to the main compiled kernel.c file (I want things to be completely modular so that other shells could be used instead, just like how we sometimes use DASH instead of DASH), and added stdio.h as a shortcut that adds all needed header files for printing to screen

---
+++ headers/stdio.h
@@ -0,0 +1,13 @@
+#ifndef _STDIO_H_
+#define _STDIO_H_
+
+
+#include "puts.h"
+#include "stdarg.h"
+#include "types.h"
+#include "sfuncts.h"
+#include "printf.h"
+#include "gets.h"
+
+
+#endif //not stdio
